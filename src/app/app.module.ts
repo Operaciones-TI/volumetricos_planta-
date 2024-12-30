133254b7a8af9cebf0669666008d6378866bf884
CarlosSv89 <CSanchez.V@tomza.com>
2024-12-30 12:14:11 -0700

    feat: get data from exel and format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,28 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { DarkmodeComponent } from './components/darkmode/darkmode.component';
+// import { DarkmodeComponent } from './components/darkmode/darkmode.component';
+import { SharedModule } from './shared/shared.module';
 import { LoadDataComponent } from './pages/load-data/load-data.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DarkmodeComponent,
     LoadDataComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     CommonModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/load-data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+const VM_HTTP_URL = 'https://api.example.com';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadDataService {
+
+  constructor() { }
+
+  
+}

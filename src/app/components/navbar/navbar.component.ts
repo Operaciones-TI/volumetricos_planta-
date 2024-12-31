f9ed470d6ad290dcbc17c59cf81525aafeb2750e
CarlosSv89 <CSanchez.V@tomza.com>
2024-12-30 17:46:24 -0700

    fix: menu animation on mobile device

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('enterAnimation', [
+      state('enter', style({ opacity: 1, transform: 'translateX(0)' })),
+      state('exit', style({ opacity: 0, transform: 'translateX(-100%)' })),
+      transition('enter => exit', animate('0.8s ease-in-out')),
+      transition('exit => enter', animate('0.8s ease-in-out'))
+    ])
+  ]
 })
+
 export class NavbarComponent implements OnInit {
   isMenuOpen: boolean = false;
+  animationState: string = 'enter';
 
   constructor(private router: Router) { }
 
@@ -20,5 +31,6 @@ export class NavbarComponent implements OnInit {
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+    this.animationState = this.animationState === 'enter' ? 'exit' : 'enter';
   }
 }

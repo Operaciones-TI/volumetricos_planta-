5ff8f5083fff10a2e9c06d3f9c0c8f51f19da03a
Jonathan Ocampo Rodriguez <JOcampo.R@tomza.com>
2025-01-08 13:03:35 -0700

    add: razon y permiso to component movimientos

--- src/app/pages/movimientos/movimientos.component.ts
+++ src/app/pages/movimientos/movimientos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { PermisoService } from 'src/app/services/permiso.service';
+import { IRazonSocial } from 'src/app/interfaces/RazonSocial.interface';
+import { IPermisos, Permiso } from 'src/app/interfaces/Permiso.interface';
 
 @Component({
   selector: 'app-movimientos',
@@ -8,10 +12,40 @@ import { Component, OnInit } from '@angular/core';
 export class MovimientosComponent implements OnInit {
   
   isDispensarios: boolean = false;
+  razonesSociales: IRazonSocial[] = [];
+  permisos: Permiso[] = [];
+  razonSelected: number = 0;
+  permisoSelected: number = 0;
 
-  constructor() { }
+  constructor(
+    private permisoService: PermisoService,
+    private toastr: ToastrService
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.getRazonesSociales();
+    await this.getPermisos(this.razonSelected);
   }
 
+  getPermisos(idRazonSocial: number) {
+    this.permisoService.getPermisos(idRazonSocial)
+    .then((permisos: Permiso[]) => {
+      console.log(permisos);
+      this.permisos = permisos;
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
+
+  async getRazonesSociales() {
+    try {
+      const data = await this.permisoService.getRazonSocialData();
+      console.log(data);
+      this.razonesSociales = data;
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
 }

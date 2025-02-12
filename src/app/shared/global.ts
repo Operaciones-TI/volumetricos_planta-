02f0a5a97dbd81fa983f6460a68bb31f46b42ef6
CarlosSv89 <CSanchez.V@tomza.com>
2025-02-12 08:18:51 -0700

    update: url to remote api

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,4 +1,4 @@
-export var Global = {
+export const Global = {
   url: 'https://controlesvolumetricos.info/api/',
   version: '1.1.4',
   url_api: 'http://volumetrics.site/api/', //https://localhost:5001/api/   http://volumetrics.site/api/
